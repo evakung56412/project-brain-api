58b156904619a862111c79275a84f49b78b52f4f
evakung56412 <eva56412@gmail.com>
2020-06-30 11:48:11 -0400

    Model

---
+++ project-brain-api/src/main/java/com/evakung/projectbrainapi/model/Brain.java
@@ -0,0 +1,52 @@
+package com.evakung.projectbrainapi.model;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(exclude = "ideas")
+public class Brain {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@JsonIgnore
+	private long id;
+	
+	@Column(unique = true, nullable = false)
+	private String email;
+	
+	@Column(unique = true, nullable = false)
+	private String username;
+	
+	// TODO: Changed to passHash when we work on security
+	@Column(unique = false, nullable = false)
+	private String password;
+	
+	// TODO: Whatever info u want
+	@Column(unique = false, nullable = true)
+	private String firstName;	
+	@Column(unique = false, nullable = true)
+	private String lastName;
+	
+	@OneToMany
+	@JsonIgnore
+	private Set<Idea> ideas;
+	
+	// This gonna be a loop, need to think if it can store to db
+//	@ManyToMany
+//	private Set<Brain> followers;
+
+}
---
+++ project-brain-api/src/main/java/com/evakung/projectbrainapi/model/Idea.java
@@ -0,0 +1,37 @@
+package com.evakung.projectbrainapi.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(exclude = "author")
+public class Idea {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@JsonIgnore
+	private long id;
+	
+	@Column(unique = false, nullable = false)
+	private String title;
+	
+	@Column(unique = false, nullable = false)
+	private String context;
+	
+	@Column(unique = false, nullable = false)
+	private String content;
+	
+	@ManyToOne
+	@JsonIgnore
+	private Brain author;
+}
