3114857a0af9c620a851f2980a3cc8d41617b880
evakung56412 <eva56412@gmail.com>
2020-07-31 00:41:05 -0400

    Revert "Rename NewIdeaForm to IdeaForm and add citeId"
    This reverts commit 49f73347501b6d25212ab9a7b70cd15153d55df5.

--- project-brain-api/src/main/java/com/evakung/projectbrainapi/form/NewIdeaForm.java
+++ project-brain-api/src/main/java/com/evakung/projectbrainapi/form/NewIdeaForm.java
@@ -3,7 +3,8 @@ package com.evakung.projectbrainapi.form;
 import lombok.Data;
 
 @Data
-public class IdeaForm {
+public class NewIdeaForm {
+	private String username;
 	private String title;
 	private String context;
 	private String content;
